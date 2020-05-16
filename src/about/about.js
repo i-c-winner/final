c18e06e9914bcca65784f528b5a46a1e9b72cc81
i-c-winner <d-belousov@hotmail.com>
2020-05-16 14:35:43 +0300

    пытаюсь сделать карусель. Попробую сделать подключение CDN

--- src/about/about.js
+++ src/about/about.js
@@ -1,1 +1,38 @@
 import "../style.css";
+
+import {
+    SWIPER
+} from '../js/utils/swiper/swiper.js'
+
+const MYSWIPER = new Swiper(('.swiper-container'), {
+    // Optional parameters
+    // direction: 'vertical',
+    // loop: true,
+
+    // If we need pagination
+    // pagination: {
+    //     el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+    on: {
+        init: function () {
+            console.log('swiper initialized');
+        },
+    },
+
+})
+console.log(SWIPER)
+console.log(MYSWIPER)
+
+
+MYSWIPER.slideNext(1000, true)
